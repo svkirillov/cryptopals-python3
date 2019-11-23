c102e69e724ee8577e25b5d214f93060109683fc
Simon Kirillov <svkirillov3@gmail.com>
2019-11-23 13:40:18 +0700

    Add challenge 18

--- cryptopals/set3/challenge18.py
+++ cryptopals/set3/challenge18.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 
+import base64
 
-def challenge18() -> None:
-    pass
+from functions.aes import AESCipher
+
+
+def challenge18() -> bool:
+    ct = base64.b64decode(
+        "L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ=="
+    )
+    result = b"Yo, VIP Let's kick it Ice, Ice, baby Ice, Ice, baby "
+
+    key = b"YELLOW SUBMARINE"
+    nonce = b"\x00" * 8
+
+    ctr = AESCipher(AESCipher.MODE_CTR, key, nonce=nonce, counter=0)
+
+    res = ctr.decrypt(ct)
+
+    return res == result
 
 
 if __name__ == "__main__":
--- tests/test_set3.py
+++ tests/test_set3.py
@@ -6,3 +6,8 @@ class TestSet3:
         from cryptopals.set3.challenge17 import challenge17
 
         assert challenge17(), "The result does not match the expected value"
+
+    def test_challenge18(self):
+        from cryptopals.set3.challenge18 import challenge18
+
+        assert challenge18(), "The result does not match the expected value"
